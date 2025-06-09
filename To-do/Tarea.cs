c4b2d3257eac3f24a97a0ac687a4995d164d27ed
Salumelian1 <salustianomeliann@gmail.com>
2025-06-09 18:26:17 -0300

    primer commit

---
+++ To-do/Tarea.cs
@@ -0,0 +1,16 @@
+namespace Tareas
+{
+    public class tareas
+    {
+        public int TareaID { get; set; }
+        public string? descripcion { get; set; }
+        public int Duracion { get; set; }
+
+        public tareas(int id, string des, int dur)
+        {
+            TareaID = id;
+            descripcion = des;
+            Duracion = dur;
+        }
+    }
+}
